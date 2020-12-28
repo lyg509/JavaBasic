8bffecd1dd3f72eb3eb98d4edc0da679329906d9
leeyonggun <dydrjs252@naver.com>
2020-12-29 00:04:13 +0900

    commit

---
+++ Practice/src/MathRound.java
@@ -0,0 +1,13 @@
+
+public class MathRound {
+
+	public static void main(String[] args) {
+		double pi = 3.141592;
+		double shortPi = Math.round(pi * 1000) / 1000.0;
+		System.out.println(shortPi);
+		
+		
+
+	}
+
+}
